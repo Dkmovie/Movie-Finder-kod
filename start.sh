4920a9cd20ce8ab8b09a3087293a574ca9a44418
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2023-04-19 17:45:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/kevinnadar22/Droplink-Movie-Finder-Bot.git /Droplink-Movie-Finder-Bot
+git clone -b master https://github.com/Dkmovie/Movie-Finder-kod.git /Droplink-Movie-Finder-Bot
 cd /Droplink-Movie-Finder-Bot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
